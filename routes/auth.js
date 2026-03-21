2958533d131a2e8dae876c2825fefeb325db7f53
Leonor <8240666@estg.ipp.pt>
2026-03-21 18:54:13 +0000

    RF3: Lógica de atualização de perfil finalizada e testada

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
+
 var express = require('express');
 var router = express.Router();
-var authController = require('../controllers/authController');
+
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/middleware');
 
 router.post('/register', authController.register);
 router.get('/confirm/email', authController.confirmEmail);
 router.post('/login', authController.login);
+router.put('/profile', authMiddleware, userController.updateUserProfile);
 
 module.exports = router;
